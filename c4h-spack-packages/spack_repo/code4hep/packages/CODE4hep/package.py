123dc66f8b4d7c70d3d4a0da4e1ae151c15d789f
Viraj Nain <virajnain174@gmail.com>
2026-07-15 10:50:33 -0500

    removed repeating dependency

--- c4h-spack-packages/spack_repo/code4hep/packages/CODE4hep/package.py
+++ c4h-spack-packages/spack_repo/code4hep/packages/CODE4hep/package.py
@@ -11,16 +11,12 @@ class Code4hep(CMakePackage):
     #license("???")
     #version(???)
 
-    depends_on("c", type="build")
-    depends_on("cxx", type="build")
-    depends_on("cmake@3.23:", type="build")
     depends_on("boost@1.80.0: +program_options", when="@2026-05-28:")
     depends_on("python@3.12.4:")
-
     depends_on("podio")
     depends_on("edm4hep")
     depends_on("dd4hep")
-    depends_on("root +geom +math") #mathmore
+    depends_on("root +geom +math")
     depends_on("nlohmann-json")
     depends_on("catch2")
     depends_on("k4geo")
